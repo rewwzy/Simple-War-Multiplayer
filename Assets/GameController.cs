608818eddfa8f4c62270a07ed20104948316e417
Rewwzy <honghai3501@gmail.com>
2024-04-17 17:22:25 +0700

    attacking

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject[] Player;
+    public GameObject[] ButtonAciton;
+    public TextMeshProUGUI PLayerTurn;
     private GameObject CurrentPlayer;
+
     private int PlayerIndex = 0;
+    private bool isDoingAction = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,19 +55,48 @@ public class GameController : MonoBehaviour
             CurrentPlayer = Player[PlayerIndex];
             CurrentPlayer.transform.Find("Action").gameObject.SetActive(true);
         }
+        PLayerTurn.text = string.Format("Player {0} turn!",PlayerIndex+1);
     }
     public void DoAttackAction()
     {
+        isDoingAction = isDoingAction == true ? false : true;
+        if (isDoingAction == false)
+        {
+            for (int i = 0; i < Player.Length; i++)
+            {
+                if (i != PlayerIndex)
+                {
+                    Player[i].GetComponent<Animator>().SetTrigger("Attacked");
+                }
+            }
+            ButtonAciton[0].GetComponent<Image>().color = Color.white;
+            ButtonAciton[1].GetComponent<Button>().enabled = true;
+            ButtonAciton[2].GetComponent<Button>().enabled = true;
+            return;
+        }
+        ButtonAciton[0].GetComponent<Image>().color = Color.red;
+        ButtonAciton[1].GetComponent<Button>().enabled = false;
+        ButtonAciton[2].GetComponent<Button>().enabled = false;
+
         for (int i = 0; i < Player.Length; i++)
         {
             if (i != PlayerIndex)
             {
                 Player[i].GetComponent<Button>().enabled = true;
+                Player[i].GetComponent<Animator>().SetTrigger("CanAttack");
             }
         }
     }
     public void DoAttack(GameObject enemy)
     {
+        for (int i = 0; i < Player.Length; i++)
+        {
+            if (i != PlayerIndex)
+            {
+                //Player[i].GetComponent<Button>().enabled = true;
+                Player[i].GetComponent<Animator>().SetTrigger("Attacked");
+            }
+        }
         var current = CurrentPlayer.GetComponent<PlayerController>();
         if (current.DoAction(PlayerController.PlayerAction.Attack))
         {
@@ -74,6 +109,8 @@ public class GameController : MonoBehaviour
             {
                 Debug.Log("Your enermy under protect");
             }
+            ButtonAciton[0].GetComponent<Image>().color = Color.white;
+            isDoingAction = false;
             NextPlayer();
         }
         else
